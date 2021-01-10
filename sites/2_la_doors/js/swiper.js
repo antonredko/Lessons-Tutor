ad751a99ad61c2fdcf7f96fc3c502c03baad5aac
Anton Redko <antonredko11116@gmail.com>
2021-01-10 22:59:40 +0200

    slider at la_doors

---
+++ sites/2_la_doors/js/swiper.js
@@ -0,0 +1,8 @@
+const slider_price_tag = new Swiper('.slider_price_tag', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets'
+    },
+    loop: true,
+    grabCursor: true
+});
